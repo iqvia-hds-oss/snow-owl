b14d069cf0cfcdec7d03bb7499eee42139f9ece5
Mark Czotter <mczotter@b2international.com>
2025-02-12 16:41:59 +0100

    chore(core): remove unnecessary environment argument from identity...
    ...factory

--- core/com.b2international.snowowl.identity.ldap/src/com/b2international/snowowl/identity/ldap/LdapIdentityPlugin.java
+++ core/com.b2international.snowowl.identity.ldap/src/com/b2international/snowowl/identity/ldap/LdapIdentityPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 B2i Healthcare, https://b2ihealthcare.com
+ * Copyright 2020-2025 B2i Healthcare, https://b2ihealthcare.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ package com.b2international.snowowl.identity.ldap;
 import com.b2international.snowowl.core.identity.IdentityProvider;
 import com.b2international.snowowl.core.identity.IdentityProviderFactory;
 import com.b2international.snowowl.core.plugin.Component;
-import com.b2international.snowowl.core.setup.Environment;
 import com.b2international.snowowl.core.setup.Plugin;
 
 /**
@@ -28,7 +27,7 @@ import com.b2international.snowowl.core.setup.Plugin;
 public final class LdapIdentityPlugin extends Plugin implements IdentityProviderFactory<LdapIdentityProviderConfig> {
 
 	@Override
-	public IdentityProvider create(Environment env, LdapIdentityProviderConfig configuration) throws Exception {
+	public IdentityProvider create(LdapIdentityProviderConfig configuration) throws Exception {
 		return new LdapIdentityProvider(configuration);
 	}
 
