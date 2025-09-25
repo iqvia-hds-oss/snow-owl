45489f47271aa22c98656e69a79a95a093bca4f0
Mark Czotter <mczotter@b2international.com>
2025-09-25 18:03:31 +0200

    feat(suggest): implement a more sophisticated lexical similarity alg...
    ...based on the available capabilities of the current index mapping

---
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/search/LexicalSimilarityTermFilter.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2025 B2i Healthcare, https://b2ihealthcare.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.core.request.search;
+
+import static com.b2international.index.query.Expressions.dismaxWithScoreCategories;
+import static com.b2international.index.query.Expressions.matchBooleanPrefix;
+import static com.b2international.index.query.Expressions.matchTextAll;
+import static com.b2international.index.query.Expressions.matchTextAny;
+
+import java.util.Set;
+
+import com.b2international.commons.exceptions.BadRequestException;
+import com.b2international.index.query.Expression;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * @since 9.8.0
+ */
+public final class LexicalSimilarityTermFilter extends TermFilter {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String term;
+	private final Integer minShouldMatch;
+	
+	private final Boolean ignoreStopwords;
+	private final boolean caseSensitive;
+	private final Boolean synonyms;
+	
+	private final String fuzziness;
+	private final Integer prefixLength;
+	private final Integer maxExpansions;
+	
+	LexicalSimilarityTermFilter(final String term, final Integer minShouldMatch, final Boolean ignoreStopwords, final boolean caseSensitive, final Boolean synonyms, final String fuzziness, final Integer prefixLength, final Integer maxExpansions) {
+		if (term == null) {
+			throw new BadRequestException("'term' filter parameter was null.");
+		}
+		this.term = term.trim();
+		this.minShouldMatch = minShouldMatch;
+		this.ignoreStopwords = ignoreStopwords;
+		this.caseSensitive = caseSensitive;
+		this.synonyms = synonyms;
+		this.fuzziness = fuzziness;
+		this.prefixLength = prefixLength;
+		this.maxExpansions = maxExpansions;
+	}
+	
+	public String getTerm() {
+		return term;
+	}
+	
+	public Integer getMinShouldMatch() {
+		return minShouldMatch;
+	}
+	
+	public Boolean isIgnoreStopwords() {
+		return ignoreStopwords;
+	}
+	
+	public boolean isCaseSensitive() {
+		return caseSensitive;
+	}
+	
+	public Boolean isSynonyms() {
+		return synonyms;
+	}
+	
+	public String getFuzziness() {
+		return fuzziness;
+	}
+	
+	public Integer getPrefixLength() {
+		return prefixLength;
+	}
+	
+	public Integer getMaxExpansions() {
+		return maxExpansions;
+	}
+	
+	@JsonIgnore
+	public boolean isAnyMatch() {
+		return getMinShouldMatch() != null;
+	}
+	
+	public LexicalSimilarityTermFilter withIgnoreStopwords() {
+		return new Builder(this).ignoreStopwords(true).build();
+	}
+	
+	public LexicalSimilarityTermFilter withTerm(String newTerm) {
+		return new Builder(this).term(newTerm).build();
+	}
+	
+	@Override
+	public Set<String> getTerms() {
+		return Set.of(term);
+	}
+	
+	@Override
+	public Expression toExpression(String field, String textFieldSuffix, String exactFieldSuffix, String prefixFieldSuffix) {
+		// lexical similarity based on the following queries
+		return dismaxWithScoreCategories(
+			// exact match overrides anything and should be scored the highest
+			TermFilter.exact().term(getTerm()).caseSensitive(isCaseSensitive()).build().toExpression(field, textFieldSuffix, exactFieldSuffix, prefixFieldSuffix),
+
+			// matching based on synonyms and tokenized text, less score than exact but more than the other (??? not sure for fuzziness though)
+			matchTextAll(fieldAlias(field, textFieldSuffix), getTerm())
+				.withIgnoreStopwords(isIgnoreStopwords())
+				.withSynonymsEnabled(isSynonyms()),
+			matchBooleanPrefix(fieldAlias(field, textFieldSuffix), getTerm())
+				.withIgnoreStopwords(isIgnoreStopwords())
+				.withSynonymsEnabled(isSynonyms()),
+
+			// matching based on fuzziness should receive higher scores than leaving out words
+			matchTextAll(fieldAlias(field, textFieldSuffix), getTerm()).withFuzziness(fuzziness, prefixLength, maxExpansions),
+			
+			// leaving out words but still matching some completely should generate a better similarity than prefix only
+			matchTextAny(fieldAlias(field, textFieldSuffix), getTerm(), getMinShouldMatch())
+				.withIgnoreStopwords(isIgnoreStopwords())
+				.withSynonymsEnabled(isSynonyms()),
+
+			// then all prefixes match
+			matchTextAll(fieldAlias(field, prefixFieldSuffix), getTerm())
+				.withIgnoreStopwords(isIgnoreStopwords()),
+				
+			// and last some prefixes match
+			matchTextAny(fieldAlias(field, prefixFieldSuffix), getTerm(), getMinShouldMatch())
+				.withIgnoreStopwords(isIgnoreStopwords())
+				.withSynonymsEnabled(isSynonyms())
+		);
+	}
+	
+	public static final class Builder {
+		
+		private String term;
+		private Integer minShouldMatch;
+		
+		private Boolean ignoreStopwords;
+		private boolean caseSensitive;
+		private Boolean synonyms;
+		
+		private String fuzziness;
+		private Integer prefixLength;
+		private Integer maxExpansions;
+		
+		Builder() {
+		}
+		
+		Builder(LexicalSimilarityTermFilter from) {
+			this.term = from.getTerm();
+			this.minShouldMatch = from.getMinShouldMatch();
+			this.ignoreStopwords = from.isIgnoreStopwords();
+			this.caseSensitive = from.isCaseSensitive();
+			this.synonyms = from.isSynonyms();
+			this.fuzziness = from.getFuzziness();
+			this.prefixLength = from.getPrefixLength();
+			this.maxExpansions = from.getMaxExpansions();
+		}
+		
+		public Builder term(String term) {
+			this.term = term;
+			return this;
+		}
+		
+		public Builder minShouldMatch(Integer minShouldMatch) {
+			this.minShouldMatch = minShouldMatch;
+			return this;
+		}
+		
+		public Builder ignoreStopwords(Boolean ignoreStopwords) {
+			this.ignoreStopwords = ignoreStopwords;
+			return this;
+		}
+		
+		public Builder caseSensitive(boolean caseSensitive) {
+			this.caseSensitive = caseSensitive;
+			return this;
+		}
+		
+		public Builder synonyms(Boolean synonyms) {
+			this.synonyms = synonyms;
+			return this;
+		}
+		
+		public Builder fuzzy() {
+			return fuzziness("AUTO");
+		}
+		
+		public Builder fuzziness(String fuzziness) {
+			this.fuzziness = fuzziness;
+			return this;
+		}
+		
+		public Builder prefixLength(Integer prefixLength) {
+			this.prefixLength = prefixLength;
+			return this;
+		}
+		
+		public Builder maxExpansions(Integer maxExpansions) {
+			this.maxExpansions = maxExpansions;
+			return this;
+		}
+		
+		public LexicalSimilarityTermFilter build() {
+			return new LexicalSimilarityTermFilter(term, minShouldMatch, ignoreStopwords, caseSensitive, synonyms, fuzziness, prefixLength, maxExpansions);
+		}
+
+	}
+	
+}
---
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/request/suggest/LexicalSimilarityConceptSuggester.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2025 B2i Healthcare, https://b2ihealthcare.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.snowowl.core.request.suggest;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.b2international.commons.http.ExtendedLocale;
+import com.b2international.snowowl.core.codesystem.CodeSystemRequests;
+import com.b2international.snowowl.core.events.util.Promise;
+import com.b2international.snowowl.core.identity.User;
+import com.b2international.snowowl.core.plugin.Component;
+import com.b2international.snowowl.core.request.SearchIndexResourceRequest;
+import com.b2international.snowowl.core.request.search.TermFilter;
+import com.b2international.snowowl.eventbus.IEventBus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+/**
+ * @since 9.8.0
+ */
+@Component
+@JsonTypeName("lexical")
+public class LexicalSimilarityConceptSuggester implements ConceptSuggester {
+
+	/**
+	 * The number of most frequent tokens (words) to consider from the selected like text corpus. Default value is 9.
+	 */
+	@JsonProperty
+	private Integer topTokenCount = 9;
+	
+	/**
+	 * The number of most frequent tokens (topTokenCount) to match in order to be accepted as a suggestion. Default value is 3. 
+	 */
+	@JsonProperty
+	private Integer minOccurenceCount = 3;
+	
+	/**
+	 * The minimum length for a token to be considered as potential top token. Default value is 0, accepting all token lengths.
+	 */
+	@JsonProperty
+	private int minTokenLength = 0;
+	
+	/**
+	 * Whether to ignore any stopwords in the selected text corpus or not. Default value is <code>true</code>.
+	 */
+	@JsonProperty
+	private boolean ignoreStopwords = true;
+	
+	/**
+	 * Whether to perform english word stemming on the tokens or not. Default value is false.
+	 */
+	@JsonProperty
+	private boolean stemming = false;
+	
+	/**
+	 * Whether to allow top tokens to fuzzy match among candidates. Default value is false. 
+	 */
+	@JsonProperty
+	private boolean fuzzy = false;
+	
+	@Override
+	public Promise<Suggestions> suggest(ConceptSuggestionContext context, int limit, String display,
+			List<ExtendedLocale> locales) {
+		// gather top tokens from the context
+		List<String> topTokens = context.topTokens(topTokenCount, minTokenLength, stemming);
+		
+		// if there are no tokens to search for then shortcut here
+		if (topTokens.isEmpty()) {
+			return Promise.immediate(new Suggestions(topTokens, limit, 0));
+		}
+		
+		final TermFilter termFilter = TermFilter.lexicalSimilarity()
+				.term(String.join(" ", topTokens))
+				.minShouldMatch(Math.min(minOccurenceCount, topTokens.size()))
+				.ignoreStopwords(ignoreStopwords)
+				.fuzziness(fuzzy ? "AUTO" : null)
+				// TODO make fuzziness options configurable in term suggester settings
+				.build();
+		
+		// get the ECL query of the from code system
+		final String inclusionQuery = context.getInclusionQueries();
+		// get the dynamically computed exclusion query set
+		final Collection<String> exclusionQueries = context.exclusionQuery(context.from().getResourceUri());
+		
+		return CodeSystemRequests.prepareSearchConcepts()
+				// always return active concepts only
+				// TODO support in suggest API settings?
+				.filterByActive(true)
+				// configure from, resource and optional ECL query
+				.filterByCodeSystemUri(context.from().getResourceUri())
+				.filterByQuery(inclusionQuery)
+				// make sure we won't suggest the same concepts as defined in like and unlike arrays (for the same code system)
+				.filterByExclusions(exclusionQueries.isEmpty() ? null : exclusionQueries)
+				// configure lexical match as basis of suggestion
+				.filterByTerm(termFilter)
+				// configure display, limit and locales
+				.setPreferredDisplay(display)
+				.setLimit(limit)
+				.setLocales(locales)
+				// always order by score
+				.sortBy(SearchIndexResourceRequest.SCORE)
+				.build()
+				.async(Map.of(User.class, context.service(User.class)))
+				.execute(context.service(IEventBus.class))
+				.then(concepts -> {
+					return new Suggestions(topTokens, concepts.getItems(), concepts.getSearchAfter(), limit, concepts.getTotal());
+				});
+	}
+	
+}
