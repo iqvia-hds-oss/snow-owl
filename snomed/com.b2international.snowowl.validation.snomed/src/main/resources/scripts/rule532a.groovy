22478aa1d52368d147226dc8b513d9ac24d593cc
Mark Czotter <mczotter@b2international.com>
2026-03-02 16:17:59 +0100

    feat(validation): update rule532a and rule532b to avoid reporting...
    ...duplicate descriptions with inactive CNC indicators
    Instead filter them out if they belong to an inactive concept.

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule532a.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule532a.groovy
@@ -8,27 +8,44 @@ import com.b2international.index.query.Expressions.ExpressionBuilder
 import com.b2international.index.revision.RevisionSearcher
 import com.b2international.snowowl.core.ComponentIdentifier
 import com.b2international.snowowl.core.date.EffectiveTimes
+import com.b2international.snowowl.core.domain.BranchContext
 import com.b2international.snowowl.snomed.common.SnomedRf2Headers
 import com.b2international.snowowl.snomed.common.SnomedConstants.Concepts
 import com.b2international.snowowl.snomed.core.domain.SnomedConcept
+import com.b2international.snowowl.snomed.core.domain.SnomedConcepts
 import com.b2international.snowowl.snomed.core.domain.SnomedDescription
 import com.b2international.snowowl.snomed.core.domain.refset.SnomedReferenceSetMember
+import com.b2international.snowowl.snomed.datastore.index.entry.SnomedConceptDocument
 import com.b2international.snowowl.snomed.datastore.index.entry.SnomedDescriptionIndexEntry
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests
 import com.google.common.collect.Lists
 
 //FSN terms should not duplicate other FSN terms, regardless of case
 
+BranchContext ctx = ctx
 RevisionSearcher searcher = ctx.service(RevisionSearcher.class)
 
 List<ComponentIdentifier> issues = Lists.newArrayList()
 
 Set<String> extensionModules = SnomedRequests.prepareSearchConcept()
 	.filterByEcl(params.modules)
-	.all()
-	.build()
-	.execute(ctx)
-	.collect({ SnomedConcept c -> c.getId() })
+	.setFields(SnomedConceptDocument.Fields.ID)
+	.setLimit(ctx.getPageSize())
+	.stream(ctx)
+	.flatMap({ SnomedConcepts c -> c.stream() })
+	.map({ SnomedConcept c -> c.getId() })
+	.toSet()
+	
+Set<String> inactiveConceptIds = SnomedRequests.prepareSearchConcept()
+	.filterByActive(false)
+	.setFields(SnomedConceptDocument.Fields.ID)
+	.setLimit(ctx.getPageSize())
+	.stream(ctx)
+	.flatMap({ SnomedConcepts c -> c.stream() })
+	.map({ SnomedConcept c -> c.getId() })
+	.toSet()
+	
+println inactiveConceptIds
 	
 def pendingMoveDescriptions = SnomedRequests.prepareSearchMember()
 	.filterByRefSet(Concepts.REFSET_DESCRIPTION_INACTIVITY_INDICATOR)
@@ -44,13 +61,15 @@ def pendingMoveDescriptions = SnomedRequests.prepareSearchMember()
 ExpressionBuilder filterExpressionBuilder = Expressions.bool()
 		.filter(SnomedDescriptionIndexEntry.Expressions.active())
 		.filter(SnomedDescriptionIndexEntry.Expressions.type(Concepts.FULLY_SPECIFIED_NAME))
-		.should(SnomedDescriptionIndexEntry.Expressions.ids(pendingMoveDescriptions)) // either pending move or no description inactivity indicator
+		// allow pending move indicators in any cases, regardless of whether the concept is active or not
+		.should(SnomedDescriptionIndexEntry.Expressions.ids(pendingMoveDescriptions))
+		// disallow any other description to be reported from inactive concepts
 		.should(Expressions.bool()
-			.mustNot(SnomedDescriptionIndexEntry.Expressions.activeMemberOf(Concepts.REFSET_DESCRIPTION_INACTIVITY_INDICATOR))
+			.mustNot(SnomedDescriptionIndexEntry.Expressions.concepts(inactiveConceptIds))
 			.build())
 
 Aggregation<String[]> activeDescriptionsByExactTerm = searcher.aggregate(
-		AggregationBuilder.bucket("rule532a", String[].class, SnomedDescriptionIndexEntry.class)
+		AggregationBuilder.bucket("rule532a_" + ctx.path(), String[].class, SnomedDescriptionIndexEntry.class)
 			.query(filterExpressionBuilder.build())
 			.onFieldValue(SnomedDescriptionIndexEntry.Fields.TERM_EXACT)
 			.fields(SnomedDescriptionIndexEntry.Fields.ID,
