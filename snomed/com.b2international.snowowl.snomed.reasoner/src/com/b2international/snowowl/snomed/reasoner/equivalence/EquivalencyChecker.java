c10fd6b5118ea139e3e9dbfcc7d061f73b11b31b
AAAlinaaa <aalaswadi@b2international.com>
2026-03-04 15:50:12 +0100

    SO-6557: Use context defined page size as limit

--- snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/equivalence/EquivalencyChecker.java
+++ snomed/com.b2international.snowowl.snomed.reasoner/src/com/b2international/snowowl/snomed/reasoner/equivalence/EquivalencyChecker.java
@@ -73,7 +73,7 @@ public final class EquivalencyChecker extends ClassifyOperation<LongKeyLongMap> 
 		
 		final List<EquivalentConceptSet> equivalentConceptSets = ClassificationRequests.prepareSearchEquivalentConceptSet()
 				.filterByClassificationId(classificationTask.getId())
-				.setLimit(10_000)
+				.setLimit(context.getPageSize())
 				.streamAsync(getEventBus(), b -> b.build(repositoryId))
 				.flatMap(conceptSets -> conceptSets.stream())
 				.collect(Collectors.toList());
