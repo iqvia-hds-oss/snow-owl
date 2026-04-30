c75c5f463668b3dcd829cae963519e02ef450871
Mark Czotter <mczotter@b2international.com>
2026-04-30 16:51:19 +0200

    feat(fhir): rewrite how we check and allow a FHIR resource to be...
    ...imported in FhirWriteSupport
    
    Instead of raising errors here, a check result will be returned and the
    caller can determine whether the result should be reported as an error
    response or just reported somewhere else.

--- fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/request/conceptmap/FhirWriteSupport.java
+++ fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/request/conceptmap/FhirWriteSupport.java
@@ -16,11 +16,13 @@
 package com.b2international.snowowl.fhir.core.request.conceptmap;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import com.b2international.snowowl.core.ResourceURI;
 import com.b2international.snowowl.core.ServiceProvider;
 import com.b2international.snowowl.core.date.EffectiveTimes;
 import com.b2international.snowowl.core.request.ResourceRequests;
+import com.b2international.snowowl.core.version.Version;
 import com.b2international.snowowl.fhir.core.exceptions.BadRequestException;
 
 /**
@@ -29,70 +31,73 @@ import com.b2international.snowowl.fhir.core.exceptions.BadRequestException;
 public interface FhirWriteSupport {
 
 	/**
-	 * Checks whether a business version can be created based on an incoming resource snapshot state (FHIR Resource representation).
+	 * @since 10.1.0
+	 */
+	enum BusinessVersionCheckResult {
+
+		/**
+		 * Version described either by version or effective date is not present in the system, write operation can proceed
+		 */
+		VERSION_NOT_PRESENT,
+
+		/**
+		 * Version present in the system with the same version identifier, write operation can patch or overwrite if requested
+		 */
+		VERSION_EXISTS,
+
+		/**
+		 * Newer version present in the system, write operation should raise an error
+		 */
+		NEWER_VERSION_EXISTS
+	}
+	
+	/**
+	 * Checks whether a FHIR resource snapshot with an optional version and/or date can be imported based on the current latest version.
 	 * 
 	 * @param context
-	 * @param resourceUri
-	 * @param newVersionToCreate
-	 * @param newVersionEffectiveDate
-	 * @return a boolean value indicating whether the version can be created either forcefully or via the standard way.
-	 * @throws BadRequestException - if the version creation cannot be completed for any reason
+	 * @param resourceUri - the resource to import the version for
+	 * @param newVersionToCreate - optional version identifier present in the FHIR resource
+	 * @param newVersionEffectiveDate - optional date value present in the FHIR resource or date value provided during import
+	 * @return a check result that describes how to proceed with the write operation
 	 */
-	default boolean checkBusinessVersion(
-			final ServiceProvider context, 
-			final ResourceURI resourceUri, 
-			final String newVersionToCreate, 
-			final LocalDate newVersionEffectiveDate) {
-			
-		return ResourceRequests.prepareSearchVersion()
-			.setLimit(1)
-			.filterByResource(resourceUri)
-			.sortBy("effectiveTime:desc")
-			.buildAsync()
-			.execute(context)
-			.first()
-			.map(latestVersion -> {
-				final int newVersionEffectiveDateCompareValue = newVersionEffectiveDate.compareTo(latestVersion.getEffectiveTime());
-				
-				// disallow importing versions with earlier effective date
-				if (newVersionEffectiveDateCompareValue < 0) {
-					throw new BadRequestException(String.format("A version for effective time '%s' already exists, can't add content with effective time '%s'.",
-						EffectiveTimes.format(latestVersion.getEffectiveTime()),
-						EffectiveTimes.format(newVersionEffectiveDate))
-					);
-				}
-				
-				// the incoming version uses the same effective date value, allow patching only when the version value matches
-				if (newVersionEffectiveDateCompareValue == 0) {
-					if (!newVersionToCreate.equals(latestVersion.getVersion())) {
-						throw new BadRequestException(String.format("A different version ('%s') is already using the given effective time '%s' value.",
-							latestVersion.getVersion(),
-							EffectiveTimes.format(newVersionEffectiveDate))
-						);
-					}
-					
-					// if the same effective date and version is given, then allow recreating the version forcefully
-					return true;
-				}
-				
-				// in all other cases check whether the version id is already taken
-				final boolean versionAlreadyExists = ResourceRequests.prepareSearchVersion()
+	default BusinessVersionCheckResult checkBusinessVersion(
+			final ServiceProvider context,
+			final ResourceURI resourceUri,
+			final Optional<String> newVersionToCreate,
+			final Optional<LocalDate> newVersionEffectiveDate) {
+		
+		return newVersionToCreate
+			// check version ID presence first
+			.map(versionId -> {
+				var versionPresent = ResourceRequests.prepareSearchVersion()
 						.setLimit(0)
-						.filterByResource(resourceUri)
-						.filterByVersionId(newVersionToCreate)
+						.filterByVersionId(versionId)
 						.buildAsync()
 						.execute(context)
 						.getTotal() > 0;
-
-				if (versionAlreadyExists) {
-					throw new BadRequestException(String.format("A version with identifier '%s' already exists.", newVersionToCreate));
+				return versionPresent ? BusinessVersionCheckResult.VERSION_EXISTS : null;
+			})
+			// if no version id, or no version entry with this version id, we need to check if the desired effective date is earlier than the current latest version
+			.orElseGet(() -> {
+				
+				if (newVersionEffectiveDate.isPresent()) {
+					final Optional<Version> latestVersion = ResourceRequests.prepareSearchVersion()
+						.one()
+						.filterByResource(resourceUri)
+						.sortBy("effectiveTime:desc")
+						.buildAsync()
+						.execute(context)
+						.first();
+					
+					if (latestVersion.isPresent() && !latestVersion.get().getEffectiveTime().isBefore(newVersionEffectiveDate.get())) {
+						return BusinessVersionCheckResult.NEWER_VERSION_EXISTS;
+					}
+					
 				}
 				
-				// if not, allow version creation
-				return false;
-			})
-			// no version present for this resource, let version create proceed without the need for force creation
-			.orElse(false);
+				// if there is no newer effective date or the resource just did not advertize any version information that we can rely on we can import it (version creation is up to the caller in this case)
+				return BusinessVersionCheckResult.VERSION_NOT_PRESENT;
+			});
 	}
 	
 }
