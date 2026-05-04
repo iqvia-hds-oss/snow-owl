de2eff0f4be6b8c668c6344f6d0c8e18b0916698
Aaalinaaa <aalaswadi@b2international.com>
2026-05-04 15:12:38 +0200

    fix: simplify rule 80 implementation
    * add streaming to core concept fetching
    * eliminate false positives in unpublished only mode
    * gain necessary information form concept documents alone

--- snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule80.groovy
+++ snomed/com.b2international.snowowl.validation.snomed/src/main/resources/scripts/rule80.groovy
@@ -1,7 +1,5 @@
 package scripts;
 
-import static com.b2international.snowowl.snomed.common.SnomedConstants.Concepts.REFSET_OWL_AXIOM
-
 import com.b2international.index.query.Expressions
 import com.b2international.index.query.Query
 import com.b2international.index.query.Expressions.ExpressionBuilder
@@ -11,64 +9,58 @@ import com.b2international.snowowl.core.date.EffectiveTimes
 import com.b2international.snowowl.snomed.common.SnomedConstants.Concepts
 import com.b2international.snowowl.snomed.core.domain.SnomedConcept
 import com.b2international.snowowl.snomed.datastore.index.entry.*
+import com.google.common.collect.HashMultimap
 import com.google.common.collect.Lists
-
+import com.google.common.collect.Multimap
 
 RevisionSearcher searcher = ctx.service(RevisionSearcher.class)
-	
+
 List<ComponentIdentifier> issues = Lists.newArrayList();
 
 ExpressionBuilder filterExpressionBuilder = Expressions.bool()
 	.filter(SnomedComponentDocument.Expressions.active())
 	.filter(SnomedComponentDocument.Expressions.modules([Concepts.MODULE_SCT_CORE, Concepts.MODULE_SCT_MODEL_COMPONENT]))
 
-
 if (params.isUnpublishedOnly) {
 	filterExpressionBuilder.filter(SnomedDocument.Expressions.effectiveTime(EffectiveTimes.UNSET_EFFECTIVE_TIME))
 }
-Set<String> coreConceptIds = searcher
-		.search(
+
+searcher.stream(
+	Query.select(Object.class)
+		.from(SnomedConceptDocument.class)
+		.fields(SnomedConceptDocument.Fields.ID, SnomedConceptDocument.Fields.PARENTS, SnomedConceptDocument.Fields.STATED_PARENTS)
+		.where(filterExpressionBuilder.build())
+		.limit(10_000)
+		.build())
+.each { hits ->
+	Multimap<String, String> parentMap = HashMultimap.create();
+	Set<String> conceptIds = [];
+	hits.each { hit ->
+		final String id = hit.id;
+		List<String> parentIds = hit.parents.collect { String.valueOf(it) }
+		List<String> statedParentIds = hit.statedParents.collect { String.valueOf(it) }
+		parentIds.each { parentMap.put(it, id) };
+		statedParentIds.each { parentMap.put(it, id) };
+		conceptIds.add(id);
+	}
+
+	Set<String> coreParents =
+		searcher.search(
 			Query.select(String.class)
 				.from(SnomedConceptDocument.class)
 				.fields(SnomedConceptDocument.Fields.ID)
-				.where(filterExpressionBuilder.build())
-				.limit(Integer.MAX_VALUE)
-			.build()
-		).toSet()
-		
-Set<String> coreConceptsWithCoreParent = searcher.search(Query.select(String.class)
-	.from(SnomedRelationshipIndexEntry.class)
-	.fields(SnomedRelationshipIndexEntry.Fields.SOURCE_ID)
-	.where(Expressions.bool()
-		.filter(SnomedRelationshipIndexEntry.Expressions.active())
-		.filter(SnomedRelationshipIndexEntry.Expressions.typeId(Concepts.IS_A))
-		.filter(SnomedRelationshipIndexEntry.Expressions.sourceIds(coreConceptIds))
-		.filter(SnomedRelationshipIndexEntry.Expressions.destinationIds(coreConceptIds))
-		.build()
-	)
-	.limit(Integer.MAX_VALUE)
-	.build()
-).toSet()
-
-searcher.search(Query.select(String.class)
-	.from(SnomedRefSetMemberIndexEntry.class)
-	.fields(SnomedRefSetMemberIndexEntry.Fields.REFERENCED_COMPONENT_ID)
-	.where(Expressions.bool()
-		.filter(SnomedRefSetMemberIndexEntry.Expressions.active())
-		.filter(SnomedRefSetMemberIndexEntry.Expressions.refsetIds([REFSET_OWL_AXIOM]))
-		.filter(SnomedRefSetMemberIndexEntry.Expressions.referencedComponentIds(coreConceptIds))
-		.filter(SnomedRefSetMemberIndexEntry.Expressions.owlExpressionType([Concepts.IS_A]))
-		.filter(SnomedRefSetMemberIndexEntry.Expressions.owlExpressionDestination(coreConceptIds))
-		.build())
-	.limit(Integer.MAX_VALUE)
-	.build()
-).each { coreConceptsWithCoreParent.add(it) }
-
-coreConceptIds.remove(Concepts.ROOT_CONCEPT) // do NOT report ROOT concept without any ISA
-coreConceptIds.each({ coreConceptId ->
-	if (!coreConceptsWithCoreParent.contains(coreConceptId)) {
-		issues.add(ComponentIdentifier.of(SnomedConcept.TYPE, coreConceptId))
-	} 
-})
+				.where(Expressions.bool()
+					.filter(SnomedConceptDocument.Expressions.ids(parentMap.keySet()))
+					.filter(SnomedComponentDocument.Expressions.active())
+					.filter(SnomedComponentDocument.Expressions.modules([Concepts.MODULE_SCT_CORE, Concepts.MODULE_SCT_MODEL_COMPONENT]))
+					.build())
+				.limit(parentMap.keySet().size())
+				.build()).toSet();
+	coreParents.each { conceptIds.removeAll(parentMap.get(it)) }
+	conceptIds.remove(Concepts.ROOT_CONCEPT)
+	conceptIds.each { 
+		issues.add(ComponentIdentifier.of(SnomedConcept.TYPE, it)) 
+	}
+}
 
 return issues
