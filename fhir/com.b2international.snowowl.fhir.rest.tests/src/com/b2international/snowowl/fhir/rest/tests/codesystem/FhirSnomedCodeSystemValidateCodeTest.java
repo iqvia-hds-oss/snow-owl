782e723fb4e2d5decd131618fe473d1947fa79c8
Adam Filep <adam.filep@iqvia.com>
2026-06-24 15:16:10 +0200

    test(fhir.codesystem): add test for codeable concept parameter with... ...multiple codings

--- fhir/com.b2international.snowowl.fhir.rest.tests/src/com/b2international/snowowl/fhir/rest/tests/codesystem/FhirSnomedCodeSystemValidateCodeTest.java
+++ fhir/com.b2international.snowowl.fhir.rest.tests/src/com/b2international/snowowl/fhir/rest/tests/codesystem/FhirSnomedCodeSystemValidateCodeTest.java
@@ -19,6 +19,7 @@ import static com.b2international.snowowl.test.commons.fhir.FhirApiHelpers.*;
 import static com.b2international.snowowl.test.commons.rest.RestExtensions.givenAuthenticatedRequest;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import org.hl7.fhir.r5.model.CodeableConcept;
 import org.hl7.fhir.r5.model.Coding;
 import org.junit.Test;
 
@@ -182,6 +183,46 @@ public class FhirSnomedCodeSystemValidateCodeTest extends FhirRestTest {
 			.body("parameter[1].valueUri", equalTo(SnomedTerminologyComponentConstants.SNOMED_URI_SCT))
 			.body("parameter[2].name", equalTo("version"))
 			.body("parameter[2].valueString", equalTo(SNOMEDCT_URL));
+	}
+	
+	@Test
+	public void POST_CodeSystem_$validate_code_Mixed_R5_CodeableConcept() throws Exception {
+
+		var codeableConcept = new CodeableConcept()
+			.addCoding(new Coding()
+				.setSystem(FhirModelHelpers.SNOMED_BASE_URI_STRING)
+				.setVersion(SNOMEDCT_URL)
+				.setCode(Concepts.ROOT_CONCEPT))
+			.addCoding(new Coding()
+				.setSystem(FhirModelHelpers.SNOMED_BASE_URI_STRING)
+				.setVersion(SNOMEDCT_URL)
+				.setCode("invalid1"))
+			.addCoding(new Coding()
+				.setSystem(FhirModelHelpers.SNOMED_BASE_URI_STRING)
+				.setVersion(SNOMEDCT_URL)
+				.setCode("invalid2"));
+
+		
+		var parameters = new CodeSystemValidateCodeParameters()
+			.setUrl(FhirModelHelpers.SNOMED_BASE_URI_STRING)
+			.setCodeableConcept(codeableConcept);
+
+		givenAuthenticatedRequest(FHIR_ROOT_CONTEXT)
+			.contentType(APPLICATION_FHIR_JSON_R5_0_0)
+			.accept(APPLICATION_FHIR_JSON_R5_0_0)
+			.body(toJson(parameters.getParameters()))
+			.when().post(CODESYSTEM_VALIDATE_CODE)
+			.then().assertThat()
+			.statusCode(200)
+			.body("parameter[0].name", equalTo("result"))
+			// TODO: the false result needs revision, fhir expects this to be true
+			.body("parameter[0].valueBoolean", equalTo(false))
+			.body("parameter[1].name", equalTo("system"))
+			.body("parameter[1].valueUri", equalTo(SnomedTerminologyComponentConstants.SNOMED_URI_SCT))
+			.body("parameter[2].name", equalTo("version"))
+			.body("parameter[2].valueString", equalTo("http://snomed.info/sct/900000000000207008/version/20200131"))
+			.body("parameter[3].name", equalTo("message"))
+			.body("parameter[3].valueString", equalTo("Could not find codes '[invalid1, invalid2]' in CodeSystem 'http://snomed.info/sct' version 'http://snomed.info/sct/900000000000207008/version/20200131'."));
 	}
 	
 }
