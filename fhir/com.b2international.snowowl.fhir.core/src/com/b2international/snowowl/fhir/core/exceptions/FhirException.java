27b3e1e26b98a6b33cd302031055e9d489c8c849
Adam Filep <adam.filep@iqvia.com>
2026-07-15 11:58:45 +0200

    docs(fhir.exception): add javadoc for toDetails method

--- fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/exceptions/FhirException.java
+++ fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/exceptions/FhirException.java
@@ -140,6 +140,12 @@ public class FhirException extends ApiException {
 					.setText(operationOutcomeCodeDisplay);
 	}
 	
+	/**
+	 * Returns a CodeableConcept with only the given message in the text field. This is used when an error message does not have to contain a full {@link Coding}
+	 * 
+	 * @param message
+	 * @return {@link CodeableConcept} with the given message in the text field.
+	 */
 	public static CodeableConcept toDetails(String message) {
 		return new CodeableConcept()
 					.setText(message);
