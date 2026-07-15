ac29fcef7b670805ecd29594012beb2bb44ecc2c
Adam Filep <adam.filep@iqvia.com>
2026-07-15 11:54:30 +0200

    style(fhir.core): remove empty lines

--- fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/exceptions/FhirResourceNotResolvableException.java
+++ fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/exceptions/FhirResourceNotResolvableException.java
@@ -18,7 +18,6 @@ package com.b2international.snowowl.fhir.core.exceptions;
 import org.hl7.fhir.r5.model.CanonicalType;
 import org.hl7.fhir.r5.model.ResourceType;
 
-
 /**
  * Exception thrown when a FHIR resource referenced by a canonical URL cannot be resolved.
  * <p>
@@ -34,14 +33,12 @@ import org.hl7.fhir.r5.model.ResourceType;
  * @since 10.3.0
  */
 public class FhirResourceNotResolvableException extends FhirException {
-	
-	private static final long serialVersionUID = 3L;
+
+	private static final long serialVersionUID = 1L;
 	
 	public FhirResourceNotResolvableException(ResourceType resource, CanonicalType canonicalType) {
-		
 		final String message = String.format("A usable %s with URL '%s' could not be resolved.", resource.name(), canonicalType.getValueAsString());
 		
 		super(message, org.hl7.fhir.r4.model.codesystems.OperationOutcome.MSGNOEXIST, null);
 	}
-	
 }
