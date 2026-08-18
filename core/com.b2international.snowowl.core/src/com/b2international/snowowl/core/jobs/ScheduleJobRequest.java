093b2481aa2d750cce655c52fec2749c24a61e2d
András Péteri <andras.peteri@iqvia.com>
2026-08-18 17:28:19 +0200

    feat(core): Document job scheduling conditions, remove explicit...
    ...deletion (cancellation) request; add scheduler test case with restart
    option

--- core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/jobs/ScheduleJobRequest.java
+++ core/com.b2international.snowowl.core/src/com/b2international/snowowl/core/jobs/ScheduleJobRequest.java
@@ -91,43 +91,73 @@ final class ScheduleJobRequest implements Request<ServiceProvider, String> {
 			SCHEDULE_LOCK.acquire();
 			
 			final Optional<RemoteJobEntry> existingJob = JobRequests.prepareSearch()
-					.one()
-					.filterById(id)
-					.setFields(RemoteJobEntry.Fields.ID, RemoteJobEntry.Fields.DELETED, RemoteJobEntry.Fields.STATE)
-					.build()
-					.execute(context)
-					.first();
+				.one()
+				.filterById(id)
+				.setFields(RemoteJobEntry.Fields.ID, RemoteJobEntry.Fields.DELETED, RemoteJobEntry.Fields.STATE)
+				.build()
+				.execute(context)
+				.first();
 			
 			if (existingJob.isPresent()) {
 				RemoteJobEntry job = existingJob.get();
 
-				// if cached reuse
-				if (cached) {
-					// use an existing running job entry until it is not done or when restart not requested, which kinda forms an asynchronous cache for any request 
-					if (!job.isDeleted() && (!job.isDone() || !restart)) {
+				/*
+				 * Cancellation has been requested (the job is marked as deleted), but it is
+				 * still running. We will have to wait it out because the job manager might send
+				 * notifications with the same ID for this "previous run."
+				 */
+				if (job.isDeleted() && !job.isDone()) {
+					throw new ConflictException("An existing job is present with the same '%s' key, but it is being cancelled. Please wait "
+						+ "for the operation to complete before scheduling a new job.", key);
+				}
+				
+				/*
+				 * We know that the job is either not deleted, or it is deleted but in a
+				 * terminal state. Deleted + done jobs should be indistinguishable from missing
+				 * jobs, so the rest of the checks only need to be performed if the job is not
+				 * deleted.
+				 */
+				if (!job.isDeleted()) {
+					
+					/*
+					 * In cached mode, others can join in on tracking the progress (or look at the results) 
+					 * of an existing job if it is either still running or completed without a restart request.
+					 */
+					if (cached && (!job.isDone() || !restart)) {
 						return id;
 					}
-				} else {
-					// if running, fail
+					
+					/*
+					 * We know that sharing job IDs is not possible and so a new job should be
+					 * scheduled. If a job is still running, we cannot do so due to key/ID
+					 * conflicts.
+					 */
 					if (!job.isDone()) {
 						throw new AlreadyExistsException(String.format("Job[%s]", request.getType()), key);
 					}
-					
-					// if restart not requested, fail
+	
+					/*
+					 * The previous job is in a terminal state, but if the user did not request a
+					 * restart, we cannot override the entry. Others might want to take a look at
+					 * the results first.
+					 */
 					if (!restart) {
 						throw new ConflictException("An existing job is present with the same '%s' key. Request 'restart' if the previous job can be safely overriden.", key);
 					}
 				}
 				
-				// otherwise delete the existing job and create a new one using the same key
-				if (!job.isDeleted()) {
-					JobRequests.prepareDelete(id).build().execute(context);
-				}
+				/*
+				 * Fall through to (re-)scheduling the job. The existing job is in a terminal
+				 * state and either restart was requested or the job is marked as deleted.
+				 * 
+				 * No explicit cancellation is needed as the job is in a terminal state
+				 * (succeeded, canceled or failed) at this point; scheduling again will override
+				 * existing job document contents.
+				 */
 			}
 			
 			User user;
 			
-			// override user if requested
 			if (User.isSystem(this.user)) {
 				// hidden system jobs
 				user = User.SYSTEM;
@@ -135,14 +165,17 @@ final class ScheduleJobRequest implements Request<ServiceProvider, String> {
 				// run jobs on behalf of others
 				user = UserRequests.prepareGet(this.user).build().execute(context);
 			} else {
+				// run jobs as the current user
 				user = context.service(User.class);
 			}
 			
 			RemoteJob job = new RemoteJob(id, key, description, user, context, request, autoClean);
 			job.setSystem(true);
+			
 			if (schedulingRule != null) {
 				job.setRule(schedulingRule);
 			}
+			
 			return context.service(RemoteJobTracker.class).schedule(job);
 		} finally {
 			SCHEDULE_LOCK.release();
