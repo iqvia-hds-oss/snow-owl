8f801ca78bb5c8977ef4f62ad7484d9c14148a99
Gergely Pekáry <gergely.pekary@iqvia.com>
2026-09-02 15:53:13 +0200

    feat(fhir.core): return name field instead of id

--- fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/request/FhirResourceHistoryGetRequest.java
+++ fhir/com.b2international.snowowl.fhir.core/src/com/b2international/snowowl/fhir/core/request/FhirResourceHistoryGetRequest.java
@@ -52,6 +52,7 @@ import com.b2international.snowowl.core.version.VersionDocument;
 import com.b2international.snowowl.fhir.core.FhirModelHelpers;
 import com.b2international.snowowl.fhir.core.R5ObjectFields;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
 
 import net.jodah.typetools.TypeResolver;
@@ -213,7 +214,7 @@ public abstract class FhirResourceHistoryGetRequest<T extends MetadataResource> 
 		}
 
 		// Replace identifier with internal OID field
-		if (internalFields.remove(R5ObjectFields.CodeSystem.IDENTIFIER)) {
+		if (internalFields.remove(R5ObjectFields.MetadataResource.IDENTIFIER)) {
 			internalFields.add(VersionDocument.Fields.OID);
 		}
 		
@@ -226,14 +227,14 @@ public abstract class FhirResourceHistoryGetRequest<T extends MetadataResource> 
 		}
 		
 		// If the URL field is requested, retrieve settings as well
-		if (internalFields.remove(R5ObjectFields.CodeSystem.URL)) {
+		if (internalFields.remove(R5ObjectFields.MetadataResource.URL)) {
 			internalFields.add(VersionDocument.Fields.URL);
 			// It would be better to add the FHIR URL override field specifically but this requires a schema change
 			internalFields.add(VersionDocument.Fields.SETTINGS);
 		}
 		
 		// If the version field is requested, retrieve settings and URL as well
-		if (internalFields.remove(R5ObjectFields.CodeSystem.VERSION)) {
+		if (internalFields.remove(R5ObjectFields.MetadataResource.VERSION)) {
 			internalFields.add(VersionDocument.Fields.URL);
 			// It would be better to add the FHIR version property field specifically, as in the above case
 			internalFields.add(VersionDocument.Fields.SETTINGS);
@@ -290,11 +291,21 @@ public abstract class FhirResourceHistoryGetRequest<T extends MetadataResource> 
 		return new Narrative(NarrativeStatus.EMPTY, div);
 	}
 
+	private String getName(final Map<String, Object> settings, final String id) {
+		if (settings != null) {
+			String name = (String) settings.get(R5ObjectFields.MetadataResource.NAME);
+			if (!Strings.isNullOrEmpty(name)) {
+				return name;
+			}
+		}
+		return id;
+	}
+	
 	private String getPublisher(final Map<String, Object> settings) {
 		if (settings == null) {
 			return "";
 		} else {
-			return (String) settings.getOrDefault(R5ObjectFields.CodeSystem.PUBLISHER, "");
+			return (String) settings.getOrDefault(R5ObjectFields.MetadataResource.PUBLISHER, "");
 		}
 	}
 	
@@ -385,8 +396,8 @@ public abstract class FhirResourceHistoryGetRequest<T extends MetadataResource> 
 		// see FhirModelHelpers for easy accessors for specific fields, such as tooling ID and "native" resource URI
 		entry.setUserData(R5ObjectFields.MetadataResource.UserData.INTERNAL_RESOURCE, resource);
 		
-		// We are using the raw ID of the resource as machine readable name
-		includeIfFieldSelected(R5ObjectFields.MetadataResource.NAME, resource::getId, entry::setName);
+		// In case of empty name default to id
+		includeIfFieldSelected(R5ObjectFields.MetadataResource.NAME, () -> getName(resource.getSettings(), resource.getId()), entry::setName);
 		// We have a description field available both for the resource and the version, here the one for the resource is needed
 		includeIfFieldSelected(R5ObjectFields.MetadataResource.DESCRIPTION, resource::getResourceDescription, entry::setDescription);
 		includeIfFieldSelected(R5ObjectFields.MetadataResource.TITLE, resource::getTitle, entry::setTitle);
@@ -403,8 +414,8 @@ public abstract class FhirResourceHistoryGetRequest<T extends MetadataResource> 
 		// addContact(contact) is a no-op if contact is null so we can safely call it here
 		includeIfFieldSelected(R5ObjectFields.MetadataResource.CONTACT, () -> toContactDetail(resource.getContact()), entry::addContact);
 
-		includeIfFieldSelected(R5ObjectFields.CodeSystem.URL, () -> getUrl(resource), entry::setUrl);
-		includeIfFieldSelected(R5ObjectFields.CodeSystem.VERSION, () -> getVersion(resource), entry::setVersion);
+		includeIfFieldSelected(R5ObjectFields.MetadataResource.URL, () -> getUrl(resource), entry::setUrl);
+		includeIfFieldSelected(R5ObjectFields.MetadataResource.VERSION, () -> getVersion(resource), entry::setVersion);
 		
 		expandResourceSpecificFields(context, entry, resource);
 
